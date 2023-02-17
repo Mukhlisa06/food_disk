69539330ccad2db790f2c174e946230f5efd088d
liza <mhlsmahmudova@gmail.com>
2023-02-18 01:27:50 +0500

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,79 @@
+let tabcontents = document.querySelectorAll('.tabcontent')
+let tabheader__items = document.querySelectorAll('.tabheader__items .tabheader__item')
+
+function showTabs(n) {
+    tabcontents.forEach(el => el.classList.add('hide'))
+
+    tabcontents[n].classList.remove('hide')
+    tabcontents[n].classList.add('show', 'fade')
+}
+
+showTabs(0)
+
+
+tabheader__items.forEach((item, idx) => {
+    item.onclick = () => {
+        tabheader__items.forEach(el => el.classList.remove('tabheader__item_active'))
+
+        item.classList.add('tabheader__item_active')
+
+        showTabs(idx)
+    }
+})
+
+// kkal
+let gens = document.querySelectorAll('#gender .calculating__choose-item')
+let inputs = document.querySelectorAll('.calculating__choose_medium input')
+let actBtns = document.querySelectorAll('.calculating__choose_big [data-act]')
+let resultView = document.querySelector('#result')
+
+let userData = {
+    gender: "woman",
+}
+
+
+gens.forEach(btn => {
+    btn.onclick = () => {
+        gens.forEach(el => el.classList.remove('calculating__choose-item_active'))    
+        btn.classList.add('calculating__choose-item_active')
+
+        let g = btn.getAttribute('data-g') 
+
+        userData.gender = g
+
+    }
+})
+
+inputs.forEach(inp => {
+    inp.onkeyup = () => {
+        let key = inp.id
+        let val = inp.value
+
+        userData[key] = val
+    }
+})
+
+
+actBtns.forEach(btn => {
+    btn.onclick = () => {
+        actBtns.forEach(el => el.classList.remove('calculating__choose-item_active'))    
+        btn.classList.add('calculating__choose-item_active')
+
+        let activeCount = btn.getAttribute('data-act') 
+
+        let {active, gender, weight, height, age} = userData
+
+        active = activeCount
+
+        if(gender === 'woman') {
+            let res = 655.1 + 9.563 * weight + 1.85 * height - 4.676 * age;
+
+            resultView.innerHTML = Math.round(res * active)
+        } else {
+            let res = 66.5 + 13.75 * weight + 5.003 * height - 6.775 * age
+
+            resultView.innerHTML = Math.round(res * active)
+        }
+
+    }
+})
